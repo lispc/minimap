c700620a516d468abdd5c3762991217719657eee
lispc <mycinbrin@gmail.com>
2014-06-24 16:26:51 +0800

    ported to linux

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-#g++ -O2 -std=c++11 -stdlib=libc++ map.cpp -o exe
-g++ -std=c++11 -stdlib=libc++ -shared -O2 -o map.so -fPIC map.cpp 
+clang++ -fPIC -O2 -std=c++11 -c map.cpp -o map.o
+clang++ map.o -shared -o map.so
--- server.py
+++ server.py
@@ -1,26 +1,19 @@
-#! /usr/bin/python
+#! /usr/bin/python2
 from flask import Flask
 from flask import request
-app = Flask(__name__)
 from ctypes import *
 import json
-lib = CDLL("map.so")
+lib = CDLL("./map.so")
 lib.init.res_type = c_int
-res = lib.init()
-'''
-x = 103.811516
-y = 1.2744
-s = "drive Singapore B"
-n = 10
-'''
-lib.j_query.restype = c_char_p
-lib.j_query.argtypes = [c_int,c_double,c_double,c_char_p]
+lib.init()
+lib.json_query.restype = c_char_p
+lib.json_query.argtypes = [c_int,c_double,c_double,c_char_p]
+app = Flask(__name__)
 @app.route('/q',methods=['GET'])
 def qf():
 	x = float(request.args['x'])
 	y = float(request.args['y'])
 	s = request.args['s']
-	#print x,y,s
-	res = lib.j_query(20,x,y,s)
+	res = lib.json_query(20,x,y,s)
 	return json.dumps(json.loads(res))
 app.run(debug=True)
